4fc1db8aac76c95cb13deffa28ae56e476456f37
Leide Ferreira <lmsf@icomp.ufam.edu.br>
2019-10-21 15:05:07 -0400

    New version

--- config/server.js
+++ config/server.js
@@ -2,6 +2,7 @@ var express = require('express');
 var consign =  require('consign');
 var app = express();
 
+
 app.use(express.static('./public')); //pegar imagens
 //Handlebars ------ expbs eh a funcao handlbars do slide do profeessor
 //app.engine('handlebars',expbs({defaultLayout:'main'}));
@@ -13,20 +14,25 @@ app.engine('handlebars', expbs({
 }));
 app.set('view engine','handlebars');
 app.set('views', './app/views');
+
 // Setup SASS directories
+// app.use('/js', [
+//   express.static(__dirname + './node_modules/jquery/dist'),
+//   express.static(__dirname + './node_modules/popper.js/dist/umd/'),
+//   express.static(__dirname + './node_modules/bootstrap/dist/js/')
+// ]);
 
 var sassMiddleware = require('node-sass-middleware');
 var path = require('path');
-
 //utilizando isso eu crio na pasta stylesheets o arquivo css gerado do sass 
 // scss de onde ele saiu da pasta sass
 app.use(sassMiddleware({
     src: __dirname + '/../public/scss', //Deu certo
     dest: __dirname + '/../public/stylesheets', //esses dois pontos eh pra andar pra tras
     debug: true, 
-    outputStyle: 'compressed' 
+    outputStyle: 'compressed'
   }),
-  // The static middleware must come after the sass middleware
+  // The static middleware must come after the sass middleware-
   express.static(path.join(__dirname, 'public'))
 )
 
