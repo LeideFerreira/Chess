7816d58993c7ce33e0798e88ba9a70b6fe24faef
Leide Ferreira <lmsf@icomp.ufam.edu.br>
2019-10-22 12:00:39 -0400

    Bootstrap ok

--- config/server.js
+++ config/server.js
@@ -7,6 +7,7 @@ app.use(express.static('./public')); //pegar imagens
 //Handlebars ------ expbs eh a funcao handlbars do slide do profeessor
 //app.engine('handlebars',expbs({defaultLayout:'main'}));
 
+
 const expbs = require('express-handlebars');
 app.engine('handlebars', expbs({  
     layoutsDir: __dirname + '/../app/views/layouts',
@@ -15,27 +16,30 @@ app.engine('handlebars', expbs({
 app.set('view engine','handlebars');
 app.set('views', './app/views');
 
-// Setup SASS directories
-// app.use('/js', [
-//   express.static(__dirname + './node_modules/jquery/dist'),
-//   express.static(__dirname + './node_modules/popper.js/dist/umd/'),
-//   express.static(__dirname + './node_modules/bootstrap/dist/js/')
-// ]);
+//Setup SASS directories
+
 
 var sassMiddleware = require('node-sass-middleware');
 var path = require('path');
 //utilizando isso eu crio na pasta stylesheets o arquivo css gerado do sass 
 // scss de onde ele saiu da pasta sass
+app.use('/js', [
+  express.static(__dirname + './node_modules/jquery/dist/'),
+  express.static(__dirname + './node_modules/popper.js/dist/umd/'),
+  express.static(__dirname + './node_modules/bootstrap/dist/js/'),
+  express.static(__dirname+'./public/js')
+]);
+
 app.use(sassMiddleware({
     src: __dirname + '/../public/scss', //Deu certo
-    dest: __dirname + '/../public/stylesheets', //esses dois pontos eh pra andar pra tras
+    dest: __dirname + '/../public/css', //esses dois pontos eh pra andar pra tras
     debug: true, 
-    outputStyle: 'compressed'
+    outputStyle: 'compressed',
+    prefix: '/css'
   }),
   // The static middleware must come after the sass middleware-
   express.static(path.join(__dirname, 'public'))
 )
-
 
 //Utilizando o Consign para diminuir a chamada de rotas
 //com consign
